bcd3624b0a11397d8122d35b5ba9ce75b5b8600d
Megha Prasad <megha.prasad@nagarro.com>
2020-06-15 11:45:40 +0530

    Initial scafold for employees

---
+++ NorthWindAPI/NorthWindDBContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NorthWindAPI.Model;
+
+namespace NorthWindAPI
+{
+    public class NorthWindDBContext : DbContext
+    {
+        public NorthWindDBContext(DbContextOptions<NorthWindDBContext> options):base(options)
+        {
+
+        }
+        public NorthWindDBContext()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+        public DbSet<Employee> Employees {get;set;}
+    }
+}
--- NorthWindAPI/Startup.cs
+++ NorthWindAPI/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -26,6 +27,7 @@ namespace NorthWindAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddDbContext<NorthWindDBContext>(options => options.UseSqlServer("data source =.; initial catalog =NORTHWND; integrated security = true"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
